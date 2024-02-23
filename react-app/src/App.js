98477a65174c217cba83e1b35f118eedc4f8191c
Zach Milo <zachmilo@gmail.com>
2024-02-23 16:38:36 -0500

    first commit

---
+++ react-app/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage";
+import { authenticate } from "./store/session";
+import Navigation from "./components/Navigation";
+import Homepage from "./components/Homepage";
+
+function App() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  const sessionUser = useSelector((state) => state.session.user);
+  useEffect(() => {
+    dispatch(authenticate()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
+  return (
+    <>
+      {/* <Navigation isLoaded={isLoaded} /> */}
+      {isLoaded && (
+        <Switch>
+          <Route path="/">
+            <Homepage/>
+          </Route>
+        </Switch>
+      )}
+    </>
+  );
+}
+
+export default App;
---
+++ react-app/src/components/Homepage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+
+function Homepage() {
+	return (
+		<div>
+			'YO!'
+		</div>
+	)
+}
+
+export default Homepage;
