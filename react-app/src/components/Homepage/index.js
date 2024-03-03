c6a91d2ffaefd92a97f598546ad9ab0ac50cf52a
Zach Milo <zachmilo@gmail.com>
2024-03-03 14:33:22 -0500

    updated page

--- react-app/src/components/Homepage/index.js
+++ react-app/src/components/Homepage/index.js
@@ -3,29 +3,42 @@ import { useHistory } from 'react-router-dom';
 import './Homepage.css'
 import OpenModalButton from '../OpenModalButton';
 import Form from '../Form';
-
+import { useState } from 'react';
+import cryptic from '../../images/cryptic.png'
+import cryptic2 from '../../images/cryptic2.png'
 
 function Homepage() {
-	const history = useHistory();
+    const history = useHistory();
+    // State to keep track of clicks on the homepage
+    const [clickCount, setClickCount] = useState(0);
+    // State to control the visibility of the buttons
+    const [showButtons, setShowButtons] = useState(false);
 
-	const navToMembers = () => {
-		history.push('/members')
-	}
+    const navToMembers = () => {
+        history.push('/members')
+    }
 
-	
+    // Function to handle clicks on the homepage
+    const handleHomepageClick = () => {
+        // Increment click count
+        setClickCount(prevCount => prevCount + 1);
+
+        // If the homepage has been clicked 7 times, show the buttons
+        if (clickCount + 1 === 6) {
+            setShowButtons(true);
+        }
+    }
 
     return (
-        <div className='homepage-div'>
-            <h2>Welcome to the Gateway</h2>
-			<p className='main-p-tag'>Have you been granted access?</p>
-			<div className='link-div'>
-			<OpenModalButton  className='homepage-button join' buttonText='Join' modalComponent={<Form />} />
-           
-			<button className='homepage-button members' onClick={navToMembers}>
-                Members
-            </button >
-
-			</div>
+        <div className='homepage-div' onClick={handleHomepageClick}>
+            <img className='cryptic-code' src={cryptic} alt=''/>
+			<img className='cryptic-code' src={cryptic2} alt=''/>
+            <div className='link-div' style={{display: showButtons ? 'block' : 'none'}}>
+                <OpenModalButton className='homepage-button join' buttonText='Join' modalComponent={<Form />} />
+                <button className='homepage-button members' onClick={navToMembers}>
+                    Founders
+                </button>
+            </div>
         </div>
     );
 }
--- react-app/src/components/Members/index.js
+++ react-app/src/components/Members/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import './MembersPage.css'
-
+import pfp from '../../images/image.png'
 function MembersPage() {
     return (
         <div className='main-team-div'>
             <h1>Members of the Occult</h1>
 			<div className='team-div'>
                 <div className='team-individual-div'>
-                    <img src='' alt='' />
+                    <img src={pfp} alt='' />
                     <h4 className='team-member'>Team Member</h4>
                     <p>description</p>
                 </div> 
                 <div className='team-individual-div'>
-                    <img src='' alt='' />
+                    <img src={pfp} alt='' />
                     <h4 className='team-member'>Team Member</h4>
                     <p>description</p>
                 </div> 
                 <div className='team-individual-div'>
-                <img src='' alt='' />
+                <img src={pfp} alt='' />
                 <h4 className='team-member'>Team Member</h4>
                 <p>description</p>
             </div> 
